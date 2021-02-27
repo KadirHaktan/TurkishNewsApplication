3360008feba9a5a2cc8687d33170cac278213f27
Hacktin <isues41@gmail.com>
2021-02-27 15:10:59 +0300

    a little bit refactoring

--- src/app/services/news/news.service.ts
+++ src/app/services/news/news.service.ts
@@ -2,17 +2,16 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http"
 import { Article } from '../../models/article';
 import { Observable } from 'rxjs';
+import { NewsCategories } from 'src/app/enums/news-categories';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
 
-  private API_KEY:string=""
+  private API_KEY:string="apiKey="
   private base_url:string="https://newsapi.org/v2/top-headlines?country=tr&"
-  private technologyCategory:string="category=technology&"
-  private businessCategory:string="category=business&"
-  private sportCategory:string="category=sport&"
+  private category:string="category="
 
 
   constructor(private _http:HttpClient) { }
@@ -20,18 +19,29 @@ export class NewsService {
 
 
   getTurkishPopularNews():Observable<Article>{
-    return this._http.get<Article>(this.base_url+this.API_KEY)
+    return this._http.get<Article>(this.createNewsUrl())
   }
 
   getTurkishTechNews():Observable<Article>{
-    return this._http.get<Article>(this.base_url+this.technologyCategory+this.API_KEY)
+    return this._http.get<Article>(this.createNewsUrl(NewsCategories.technology))
   }
 
   getTurkishSportNews():Observable<Article>{
-    return this._http.get<Article>(this.base_url+this.sportCategory+this.API_KEY)
+    return this._http.get<Article>(this.createNewsUrl(NewsCategories.sport))
   }
 
   getTurkishBusinessNews():Observable<Article>{
-    return this._http.get<Article>(this.base_url+this.businessCategory+this.API_KEY)
+    return this._http.get<Article>(this.createNewsUrl(NewsCategories.business))
+  }
+
+  private createNewsUrl(newsCategory?:NewsCategories):string{
+    if(newsCategory!=null){
+      return `${this.base_url}${this.category}${newsCategory}&${this.API_KEY}`
+    }
+
+    else{
+      return this.base_url+this.API_KEY
+    }
+
   }
 }
