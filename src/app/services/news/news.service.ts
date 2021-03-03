8d5214446e1c6cbb9f076675994c139a84305f65
Hacktin <isues41@gmail.com>
2021-03-03 19:49:32 +0300

    service's functions have been edited

--- src/app/services/news/news.service.ts
+++ src/app/services/news/news.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http"
+import {HttpClient, HttpHeaders} from "@angular/common/http"
 import { Article } from '../../models/article';
 import { Observable } from 'rxjs';
 import { NewsCategories } from 'src/app/enums/news-categories';
@@ -9,33 +9,19 @@ import { NewsCategories } from 'src/app/enums/news-categories';
 })
 export class NewsService {
 
-  private API_KEY:string="apiKey="
-  private base_url:string="https://newsapi.org/v2/top-headlines?country=tr&"
+  private API_KEY:string="apiKey=a0257538249c4e71a9622fa82bab68f0"
+  private base_url:string="http://newsapi.org/v2/top-headlines?country=tr&"
   private category:string="category="
 
 
   constructor(private _http:HttpClient) { }
 
-
-
-  getTurkishPopularNews():Observable<Article>{
-    return this._http.get<Article>(this.createNewsUrl())
-  }
-
-  getTurkishTechNews():Observable<Article>{
-    return this._http.get<Article>(this.createNewsUrl(NewsCategories.technology))
-  }
-
-  getTurkishSportNews():Observable<Article>{
-    return this._http.get<Article>(this.createNewsUrl(NewsCategories.sport))
-  }
-
-  getTurkishBusinessNews():Observable<Article>{
-    return this._http.get<Article>(this.createNewsUrl(NewsCategories.business))
+  getTurkishNews(newsCategory?:NewsCategories):Observable<Article>{
+    return this._http.get<Article>(this.createNewsUrl(newsCategory))
   }
 
   private createNewsUrl(newsCategory?:NewsCategories):string{
-    if(newsCategory!=null){
+    if(newsCategory!=NewsCategories.null){
       return `${this.base_url}${this.category}${newsCategory}&${this.API_KEY}`
     }
 
